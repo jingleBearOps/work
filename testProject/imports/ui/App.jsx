a510efe10c6086d5f3d0b79d1577794a462a8d2c
JingzhuoTian <Jingzhuo.Tian@gov.bc.ca>
2021-09-14 18:31:47 +0000

    add styles

--- testProject/imports/ui/App.jsx
+++ testProject/imports/ui/App.jsx
@@ -3,31 +3,40 @@ import { useTracker } from 'meteor/react-meteor-data';
 import { Task } from './Task';
 import { TasksCollection } from '/imports/api/TasksCollection';
 import { TaskForm } from './TaskForm';
-const toggoleChecked=({_id, isChecked}) => {
-  TasksCollection.update(_id,{
-    $set:{
+const toggoleChecked = ({ _id, isChecked }) => {
+  TasksCollection.update(_id, {
+    $set: {
       isChecked: !isChecked
     }
   })
 };
-const deleteTask =({_id}) => TasksCollection.remove(_id);
+const deleteTask = ({ _id }) => TasksCollection.remove(_id);
 export const App = () => {
   const tasks = useTracker(() => TasksCollection.find({}).fetch());
 
   return (
-    <div>
-      <h1>Welcome to Meteor!</h1>
+    <div className="app">
+      <header>
+        <div className="app-bar">
+          <div className="app-header">
+            <h1>right! its a TO DO LIST</h1>
+          </div>
+        </div>
+      </header>
 
-      <TaskForm/>
 
-      <ul>
-        { tasks.map(task => <Task key={ task._id }
-        task={ task }
-        onCheckboxClick={toggoleChecked}
-        onDeleteClick={deleteTask}
-        />) }
+      <div className="main">
+        <TaskForm />
 
-      </ul>
+        <ul className="tasks">
+          {tasks.map(task => <Task key={task._id}
+            task={task}
+            onCheckboxClick={toggoleChecked}
+            onDeleteClick={deleteTask}
+          />)}
+
+        </ul>
+      </div>
     </div>
   );
 };
