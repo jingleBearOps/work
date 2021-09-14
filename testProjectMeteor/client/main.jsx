f67c1e995e077cb1e922ed105337ec4718a2fcab
JingzhuoTian <Jingzhuo.Tian@gov.bc.ca>
2021-09-14 22:11:04 +0000

    add game background and styles

--- testProjectMeteor/client/main.jsx
+++ testProjectMeteor/client/main.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
 import { App } from '/imports/ui/App';
-
+import {Game} from '/game/Game';
 Meteor.startup(() => {
   console.log("Meteor has started");
   render(<App/>, document.getElementById('react-target'));
+  render(<Game/>, document.getElementById('react-game'));
 });
---
+++ testProjectMeteor/game/Game.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+class Square extends React.Component {
+    render() {
+        return (
+            <button className="square">
+                {/* TODO */}
+            </button>
+        );
+    }
+}
+
+class Board extends React.Component {
+    renderSquare(i) {
+        return <Square />;
+    }
+
+    render() {
+        const status = 'Next player: X';
+
+        return (
+            <div>
+                <div className="status">{status}</div>
+                <div className="board-row">
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
+                </div>
+            </div>
+        );
+    }
+}
+export const Game = () => {
+    return (
+        // <h1>This is the Game</h1>
+        <div className="game">
+            <div className="game-board">
+                <Board />
+            </div>
+            <div className="game-info"></div>
+        </div>
+    );
+};
